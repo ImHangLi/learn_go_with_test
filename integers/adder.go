c32af7253e6c63c510daa1631fd2263b25650109
Hang Li <hangli1010@outlook.com>
2025-01-15 18:22:57 +0800

    Finished chapter

--- integers/adder.go
+++ integers/adder.go
@@ -1,3 +1,11 @@
+/**
+What we have covered:
+- More practice of the TDD workflow
+- Integers, addition
+- Writing better documentation so users of our code can understand its usage quickly
+- Examples of how to use our code, which are checked as part of our tests
+*/
+
 package integers
 
 // Add takes two integers and returns the sum of them
