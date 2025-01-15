158236d989a08d029c2b43a85cbbc7b4c41c5081
Hang Li <hangli1010@outlook.com>
2025-01-15 16:05:33 +0800

    Final refactor

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,16 +1,45 @@
+/** 
+Some of Go's syntax around
+- Writing tests
+- Declaring functions, with arguments and return types
+- if, const and switch
+- Declaring variables and constants
+*/
+
 package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish = "Spanish"
+	french  = "French"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "Spanish"))
 }
