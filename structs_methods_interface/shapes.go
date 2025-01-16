30f38164b49c5a1108f49a595b7a89e7e967048e
Hang Li <hangli1010@outlook.com>
2025-01-16 12:06:40 +0800

    Finished chapter

--- structs_methods_interface/shapes.go
+++ structs_methods_interface/shapes.go
@@ -1,3 +1,11 @@
+/**
+We have covered:
+- Declaring structs to bundle related data together
+- Declaring interfaces so you can define functions that can be used by different types (parametric polymorphism)
+- Adding methods so you can add functionality to data types and so you can implement interfaces
+- Table driven tests to make assertions clearer and test suites easier to extend & maintain
+*/
+
 package main
 
 import "math"
@@ -21,6 +29,15 @@ type Circle struct {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
 }
 
 func Perimeter(rectangle Rectangle) float64 {
