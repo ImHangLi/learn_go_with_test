50e8f19d261d6b40d37842360803fd06c12d87b1
Hang Li <hangli1010@outlook.com>
2025-01-17 15:01:02 +0800

    Finished chapter

---
+++ dependency_injection/di.go
@@ -0,0 +1,26 @@
+/**
+We have covered:
+- Dependency injuection gives us the power to control where the data goes
+- Easier and Faster to test your code
+*/
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
